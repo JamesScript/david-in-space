ced51373ec4f880f21d7894316a3f0d17c8ca634
crossjames934 <crossjames934@gmail.com>
2018-06-02 16:58:35 +0100

    satisfied, changed to playable version

--- countAndSpawn.js
+++ countAndSpawn.js
@@ -1,5 +1,5 @@
 function CountRegister() {
-    this.currentLevel = 2; // 0 is menu
+    this.currentLevel = 0; // 0 is menu
     this.sinceLastMeteorite = 0;
     this.nextMeteorite = Math.floor(Math.random() * 300) + 50;
     this.enemyWait = 0;
@@ -89,7 +89,6 @@ function CountRegister() {
             }
         },
         2: () => {
-            shop.stock = [];
             this.interlude();
         },
         3: () => {
